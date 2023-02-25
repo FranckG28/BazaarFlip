ebc45799d7416f945cbac385d5f8a988c2f9b0a2
Franck GUTMANN <franck.gutmann@pixacare.com>
2023-02-25 01:41:13 +0100

    add get skin url

--- src/components/stone.tsx
+++ src/components/stone.tsx
@@ -1,16 +1,8 @@
 import { Item } from "@/models/item";
 import { nFormatter } from "@/services/utils";
-import { MinecraftSkinViewer } from "@yannichock/react-minecraft-skin-viewer";
 
 export default function StoneComponent({ item }: { item: Item }) {
     return <div key={item.id} className="flex flex-row gap-2">
-        <MinecraftSkinViewer
-            skin={item.skinUrl}
-            width={64}
-            height={64}
-            zoom={false}
-            background="transparent"
-        />
         <p className='flex gap-2 items-baseline'>
             <span className='text-indigo-800 font-bold'>{item.name}</span>
             <span className='rounded-full px-2 bg-green-200 text-green-800'>Buy : {nFormatter(item.buyPrice, 2)}</span>
